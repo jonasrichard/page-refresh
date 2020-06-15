52f986a3f8252a28b9845659f4afe0cd66837869
Richard Jonas <richard.jonas@derivco.se>
2020-06-15 18:35:36 +0200

    Make progress

--- background.js
+++ background.js
@@ -5,17 +5,41 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
+    chrome.storage.sync.set({refreshTabs: []}, function() {
     });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                //pageUrl: {hostEquals: 'developer.chrome.com'},
+                //pageUrl: {schemes: ['file']},
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
+    });
+
+    chrome.alarms.onAlarm.addListener(function() {
+        console.log('Alarm fired');
+        chrome.storage.sync.get('refreshInterval', function(data) {
+            console.log('Get data', data);
+        });
+    });
+
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+        chrome.storage.sync.get('refreshInterval', function(data) {
+            if (data['tab_' + activeInfo.tabId]) {
+                chrome.browserAction.setBadgeText({text: 'ON'});
+            } else {
+                chrome.browserAction.setBadgeText({});
+            }
+        });
+    });
+
+    chrome.tabs.onRemoved.addListener(function(tabId) {
+        chrome.storage.sync.get('refreshInterval', function(data) {
+            delete data['tab_' + tabId];
+        });
     });
 });
 
--- popup.js
+++ popup.js
@@ -1,15 +1,32 @@
-let changeColor = document.getElementById('changeColor');
+let setRefresh = document.getElementById('setRefresh');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('refreshInterval', function(data) {
+    //changeColor.style.backgroundColor = data.color;
+    //changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
+setRefresh.onclick = function(element) {
+    let interval = document.getElementById('refreshInterval').value;
+    console.log('Refresh page in every ' + interval + ' seconds');
+
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+        chrome.storage.sync.get('refreshInterval', function(data) {
+            let tabId = tabs[0].id;
+            let tabKey = 'tab_' + tabId;
+            let tabValue = {
+                tabId: tabId,
+                intervalInMin: interval / 60.0,
+                nextTime: new Date().getTime() + interval * 1000
+            };
+
+            data[tabKey] = tabValue;
+
+            chrome.storage.sync.set({'refreshInterval': data}, function() {
+                console.log(data);
+            });
+        });
     });
+
+    chrome.alarms.create('nextRefresh', {delayInMinutes: interval / 60.0});
 };
